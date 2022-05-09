7569079805b1bd0340fddc73508fe9ccdcb36b34
Fisz <Fisz@DESKTOP-CA7O5MK>
2022-05-09 20:37:06 -0300

    Agregar archivos de proyecto.

---
+++ Afip/src/Afip.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LibreriaAfip
+{
+    public class Afip
+    {
+
+        string WSAA_WSDL;
+        string WSAA_URL;
+        string CERT;
+        string PRIVATEKEY;
+        string PASSPHRASE;
+        string RES_FOLDER;
+        string TA_FOLDER;
+        int SOAP_VERSION;
+        long CUIT;
+        bool PRODUCTION;
+        bool EXCEPTIONS;
+        ElectronicBilling electronicBilling = new ElectronicBilling();
+
+        public Afip(string WSAA_WSDL, string WSAA_URL, string CERT, string PRIVATEKEY, string PASSPHRASE, string RES_FOLDER, string TA_FOLDER, long CUIT, bool PRODUCTION, bool EXCEPTIONS, int SOAP_VERSION)
+        {
+            this.WSAA_WSDL = WSAA_WSDL;
+            this.WSAA_URL = WSAA_URL;
+            this.CERT = CERT;
+            this.PRIVATEKEY = PRIVATEKEY;
+            this.SOAP_VERSION = SOAP_VERSION;
+            this.PASSPHRASE = PASSPHRASE;
+            this.RES_FOLDER = RES_FOLDER;
+            this.TA_FOLDER = TA_FOLDER;
+            this.CUIT = CUIT;
+            this.PRODUCTION = PRODUCTION;
+            this.EXCEPTIONS = EXCEPTIONS;
+        }
+
+    }
+    public class ElectronicBilling
+    {
+        public int executeRequest(string operation, Object[] req)
+        {
+            return 666;
+        }
+        public int getLastBill(int sales_point, int type)
+        {
+            Object[] salesPoint = new Object[2] { "ptoVenta", sales_point };
+            Object[] billType = new Object[2] { "CbteTipo", type };
+            Object[] req = new Object[2] { salesPoint, billType };
+
+
+            return this.executeRequest("FECompUltimoAutorizado", req);
+        }
+        
+    }
+
+
+        
+}
---
+++ TestAfip/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LibreriaAfip;
+namespace TestAfip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Afip afip = new Afip();
+            afip.WSAA_WSDL = "heee"
+
+
+        }
+    }
+}
