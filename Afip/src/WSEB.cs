7b69528ef4bcc9f82dffad2b664bb2e9c797e8f5
mfisz <maxifisz@gmail.com>
2022-07-04 14:17:33 -0300

    Added CreateVoucher operation template and nodes to xml fabric. Created more auxiliary methods. Created some basic error handling.

--- Afip/src/WSEB.cs
+++ Afip/src/WSEB.cs
@@ -13,48 +13,22 @@ namespace LibreriaAfip
     public class WSEB
     {
         //TODO:
-        //  - Create method/fabric for node creating, specially for CreateVoucher (nested repeteaded names), test method
-        //  - Exception handling
         //  - Logging
-        //  - Talk about input format 
-        //  - Delete local dependencies, create proper constructors for proper library use by app
 
         string mainUrl = "https://wswhomo.afip.gov.ar/wsfev1/service.asmx";
         
         public void executeOperation(Dictionary<string, string> input, string operation)
         {
-            //Input data format example
+            string templatePaths = @"E:\Source\teredev-teu\Afip\src\XMLs\" + operation + ".xml";
 
-            // XML request creation and editing
-            XmlDocument xmlRequest = new XmlDocument();
-            xmlRequest.LoadXml(File.ReadAllText(@"E:\Source\teredev-teu\Afip\src\XMLs\" + operation + ".xml"));
+            // XML request creation
+            XmlDocument xmlRequest = LoadTemplate(templatePaths);            
 
-            // XML namespace creation as "ar" prefix is present in template, maybe to be made a function later on
-            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xmlRequest.NameTable);
-            namespaceManager.AddNamespace("ar", "http://ar.gov.afip.dif.FEV1/");
-
-            // Selecting nodes and creating a list          ->                  ->                ->               ->                ->          ->   //TO DO: Create a method/fabric for the nodes
-            XmlNode xmlNodeToken = xmlRequest.SelectSingleNode("//ar:Token", namespaceManager);
-            XmlNode xmlNodeSign = xmlRequest.SelectSingleNode("//ar:Sign", namespaceManager);
-            XmlNode xmlNodeCuit = xmlRequest.SelectSingleNode("//ar:Cuit", namespaceManager);
-            XmlNode xmlNodeMonId = xmlRequest.SelectSingleNode("//ar:MonId", namespaceManager);
-            XmlNode xmlNodeCbteTipo = xmlRequest.SelectSingleNode("//ar:CbteTipo", namespaceManager);
-            XmlNode xmlNodeCbteNro = xmlRequest.SelectSingleNode("//ar:CbteNro", namespaceManager);
-            XmlNode xmlNodePtoVta = xmlRequest.SelectSingleNode("//ar:PtoVta", namespaceManager);
-
-
-            List<XmlNode> nodeList = new List<XmlNode> {xmlNodeToken, xmlNodeSign, xmlNodeCuit, xmlNodeMonId, xmlNodeCbteTipo, xmlNodeCbteNro, xmlNodePtoVta };
-            
-            // Editing nodes if they exist
-            foreach(XmlNode node in nodeList)
-            {
-                if(node != null)
-                {
-                    node.InnerText = input[node.LocalName];
-                }
-            }
+            // Adding request data to template
+            xmlRequest = EditRequest(xmlRequest, input);
 
             // XML -> usable data to create web request
+            xmlRequest.Save(@"C:\Users\54112\Desktop\" + operation + "Request.xml");
             string data = xmlRequest.OuterXml;
             HttpWebRequest webRequest = CreateWebRequest(mainUrl);
             WriteIntoHttpRequest(webRequest, data);
@@ -66,6 +40,166 @@ namespace LibreriaAfip
             XmlDocument xmlResponse = ReadFromHttpResponse(httpResponse);
             xmlResponse.Save(@"C:\Users\54112\Desktop\" + operation + "Response.xml");
         }
+
+
+        // Auxiliary methods
+        static private XmlDocument EditRequest(XmlDocument request, Dictionary<string, string> data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(request.NameTable);
+            namespaceManager.AddNamespace("ar", "http://ar.gov.afip.dif.FEV1/");
+
+            XmlNode xmlNodeToken = request.SelectSingleNode("//ar:Token", namespaceManager);
+            XmlNode xmlNodeSign = request.SelectSingleNode("//ar:Sign", namespaceManager);
+            XmlNode xmlNodeCuit = request.SelectSingleNode("//ar:Cuit", namespaceManager);
+            XmlNode xmlNodeMonId = request.SelectSingleNode("//ar:MonId", namespaceManager);
+            XmlNode xmlNodeCbteTipo = request.SelectSingleNode("//ar:CbteTipo", namespaceManager);
+            XmlNode xmlNodeCbteNro = request.SelectSingleNode("//ar:CbteNro", namespaceManager);
+            XmlNode xmlNodePtoVta = request.SelectSingleNode("//ar:PtoVta", namespaceManager);
+            XmlNode xmlNodeCantReg = request.SelectSingleNode("//ar:CantReg", namespaceManager);
+            XmlNode xmlNodeConcepto = request.SelectSingleNode("//ar:Concepto", namespaceManager);
+            XmlNode xmlNodeDocTipo = request.SelectSingleNode("//ar:DocTipo", namespaceManager);
+            XmlNode xmlNodeDocNro = request.SelectSingleNode("//ar:DocNro", namespaceManager);
+            XmlNode xmlNodeCbteDesde = request.SelectSingleNode("//ar:CbteDesde", namespaceManager);
+            XmlNode xmlNodeCbteHasta = request.SelectSingleNode("//ar:CbteHasta", namespaceManager);
+            XmlNode xmlNodeCbteFch = request.SelectSingleNode("//ar:CbteFch", namespaceManager);
+            XmlNode xmlNodeImpTotal = request.SelectSingleNode("//ar:ImpTotal", namespaceManager);
+            XmlNode xmlNodeImpTotConc = request.SelectSingleNode("//ar:ImpTotConc", namespaceManager);
+            XmlNode xmlNodeImpNeto = request.SelectSingleNode("//ar:ImpNeto", namespaceManager);
+            XmlNode xmlNodeImpOpEx = request.SelectSingleNode("//ar:ImpOpEx", namespaceManager);
+            XmlNode xmlNodeImpTrib = request.SelectSingleNode("//ar:ImpTrib", namespaceManager);
+            XmlNode xmlNodeImpIVA = request.SelectSingleNode("//ar:ImpIVA", namespaceManager);
+            XmlNode xmlNodeFchServDesde = request.SelectSingleNode("//ar:FchServDesde", namespaceManager);
+            XmlNode xmlNodeFchServHasta = request.SelectSingleNode("//ar:FchServHasta", namespaceManager);
+            XmlNode xmlNodeFchVtoPago = request.SelectSingleNode("//ar:FchVtoPago", namespaceManager);
+            XmlNode xmlNodeMonCotiz = request.SelectSingleNode("//ar:MonCotiz", namespaceManager);
+
+            List<XmlNode> nodeList = new List<XmlNode> {xmlNodeToken, xmlNodeSign, xmlNodeCuit, xmlNodeMonId, xmlNodeCbteTipo, xmlNodeCbteNro, xmlNodePtoVta,
+            xmlNodeCantReg, xmlNodeConcepto, xmlNodeDocTipo, xmlNodeDocNro, xmlNodeCbteDesde, xmlNodeCbteHasta, xmlNodeCbteFch, xmlNodeImpTotal, xmlNodeImpTotConc, xmlNodeImpNeto, xmlNodeImpOpEx,
+            xmlNodeImpTrib, xmlNodeImpIVA, xmlNodeFchServDesde, xmlNodeFchServHasta, xmlNodeFchVtoPago, xmlNodeMonCotiz };
+
+            try
+            {
+                // Editing nodes if they exist
+                foreach (XmlNode node in nodeList)
+                {
+                    if (node != null)
+                    {
+                        node.InnerText = data[node.LocalName];
+                    }
+                }                
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Data object was null", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while trying to edit XML template", ex.Message);
+            }
+            return request;
+
+        }
+        static private XmlDocument LoadTemplate(string path)
+        {
+            XmlDocument template = new XmlDocument();
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            try
+            {
+                template.LoadXml(File.ReadAllText(path));
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Template file for selected operation was not found", ex.Message);
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Template file could not be accessed", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while trying to read template XMLs", ex.Message);
+            }
+            
+            return template;
+        }
+        static private HttpWebRequest CreateWebRequest(string url)
+        {
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "text/xml;charset=utf-8";
+            return httpRequest;
+        }
+        static private void WriteIntoHttpRequest(HttpWebRequest request, string data)
+        {
+            try
+            {
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    if (data == null)
+                    {
+                        throw new ArgumentNullException();
+                    }
+                    streamWriter.Write(data);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("An argument was invalid, most probably, the data object being loaded into the web request was null or empty", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while trying to write into web request", ex.Message);
+            }
+
+        }
+        static private XmlDocument ReadFromHttpResponse(HttpWebResponse response)
+        {
+            if (response == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            XmlDocument xmlOutput = new XmlDocument();
+
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string result = streamReader.ReadToEnd();
+                    xmlOutput.LoadXml(result);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Respose was empty", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while trying to read response", ex.Message);
+            }
+
+            return xmlOutput;
+        }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -194,34 +328,5 @@ namespace LibreriaAfip
             XmlDocument xmlResponse = ReadFromHttpResponse(httpResponse);
             xmlResponse.Save(@"C:\Users\54112\Desktop\GetCurrencyPriceResponse.xml");
         }
-
-
-        // Auxiliary methods
-
-        static private HttpWebRequest CreateWebRequest(string url)
-        {
-            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpRequest.Method = "POST";
-            httpRequest.ContentType = "text/xml;charset=utf-8";
-            return httpRequest;
-        }
-        static private void WriteIntoHttpRequest(HttpWebRequest request, string data)
-        {
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                streamWriter.Write(data);
-            }
-        }
-        static private XmlDocument ReadFromHttpResponse(HttpWebResponse response)
-        {
-            XmlDocument xmlOutput = new XmlDocument();
-            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                string result = streamReader.ReadToEnd();
-                xmlOutput.LoadXml(result);
-            }
-            return xmlOutput;
-        }
-
     }
 }
--- TestAfip/Program.cs
+++ TestAfip/Program.cs
@@ -8,23 +8,48 @@ namespace TestAfip
         static void Main(string[] args)
         {
             var watch = new System.Diagnostics.Stopwatch();
+
             Dictionary<string, string> inputData = new Dictionary<string, string>();
-            inputData.Add("Token", "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgc3JjPSJDTj13c2FhaG9tbywgTz1BRklQLCBDPUFSLCBTRVJJQUxOVU1CRVI9Q1VJVCAzMzY5MzQ1MDIzOSIgZHN0PSJDTj13c2ZlLCBPPUFGSVAsIEM9QVIiIHVuaXF1ZV9pZD0iMjgyMjg4NDIwNSIgZ2VuX3RpbWU9IjE2NTY2MjI4NDUiIGV4cF90aW1lPSIxNjU2NjY2MTA1Ii8+CiAgICA8b3BlcmF0aW9uIHR5cGU9ImxvZ2luIiB2YWx1ZT0iZ3JhbnRlZCI+CiAgICAgICAgPGxvZ2luIGVudGl0eT0iMzM2OTM0NTAyMzkiIHNlcnZpY2U9IndzZmUiIHVpZD0iU0VSSUFMTlVNQkVSPUNVSVQgMjA0MjMwMjExNTMsIENOPXRvZG9lbnVub3Rlc3RpbmciIGF1dGhtZXRob2Q9ImNtcyIgcmVnbWV0aG9kPSIyMiI+CiAgICAgICAgICAgIDxyZWxhdGlvbnM+CiAgICAgICAgICAgICAgICA8cmVsYXRpb24ga2V5PSIyMDQyMzAyMTE1MyIgcmVsdHlwZT0iNCIvPgogICAgICAgICAgICA8L3JlbGF0aW9ucz4KICAgICAgICA8L2xvZ2luPgogICAgPC9vcGVyYXRpb24+Cjwvc3NvPgo=");
-            inputData.Add("Sign", "BySHekBqjl1un0BVsufqbPP3QMthAirJXw4R0j+DDU4mOPDi+Jabrf9KaSZkHzWvI7S17I5ou1OCSncWANn8oXKjrl5vVxl52RBYSAvkXuyjRgBTcE4xO8WkJd5PiP+P7H0j99MseiMVYP3nQZb1vL55aQT0VpM8X1mbCRzbrHQ=");
-            inputData.Add("Cuit", "");
-            inputData.Add("MonId", "");
-            inputData.Add("CbteTipo", "");
-            inputData.Add("CbteNro", "");
-            inputData.Add("PtoVta", "");
+            inputData.Add("Token", "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgc3JjPSJDTj13c2FhaG9tbywgTz1BRklQLCBDPUFSLCBTRVJJQUxOVU1CRVI9Q1VJVCAzMzY5MzQ1MDIzOSIgZHN0PSJDTj13c2ZlLCBPPUFGSVAsIEM9QVIiIHVuaXF1ZV9pZD0iMTA4MTY1NjM2OCIgZ2VuX3RpbWU9IjE2NTY4ODgxMjgiIGV4cF90aW1lPSIxNjU2OTMxMzg4Ii8+CiAgICA8b3BlcmF0aW9uIHR5cGU9ImxvZ2luIiB2YWx1ZT0iZ3JhbnRlZCI+CiAgICAgICAgPGxvZ2luIGVudGl0eT0iMzM2OTM0NTAyMzkiIHNlcnZpY2U9IndzZmUiIHVpZD0iU0VSSUFMTlVNQkVSPUNVSVQgMjA0MjMwMjExNTMsIENOPXRldSIgYXV0aG1ldGhvZD0iY21zIiByZWdtZXRob2Q9IjIyIj4KICAgICAgICAgICAgPHJlbGF0aW9ucz4KICAgICAgICAgICAgICAgIDxyZWxhdGlvbiBrZXk9IjIwNDIzMDIxMTUzIiByZWx0eXBlPSI0Ii8+CiAgICAgICAgICAgICAgICA8cmVsYXRpb24ga2V5PSIyMzQyNTc1ODIwOSIgcmVsdHlwZT0iNCIvPgogICAgICAgICAgICA8L3JlbGF0aW9ucz4KICAgICAgICA8L2xvZ2luPgogICAgPC9vcGVyYXRpb24+Cjwvc3NvPgo=");
+            inputData.Add("Sign", "ccFlL+HoyZHI2+PIZJpzj0Egv8MMvFvajAbgqczYySgzdIy8BBOPAIUmQKw9i/VoCRc+HOXpaRjPHMR4+kNNfoK+gV15uCkMBvvr4hT1oPN2eEm2hyXVetXh5BpciO5Jcg2UVFacBeOShDrHBLZksJy620J++5lSDi8phJ45xd4=");
+            inputData.Add("Cuit", "23425758209");
+            inputData.Add("MonId", "PES");
+            inputData.Add("CbteTipo", "6");
+            inputData.Add("CbteNro", "0");
+            inputData.Add("PtoVta", "3");
+            inputData.Add("CantReg", "1");
+            inputData.Add("Concepto", "1");
+            inputData.Add("DocTipo", "80");
+            inputData.Add("DocNro", "20423021153");
+            inputData.Add("CbteDesde", "1");
+            inputData.Add("CbteHasta", "1");
+            inputData.Add("CbteFch", "20220703");
+            inputData.Add("ImpTotal", "666");
+            inputData.Add("ImpTotConc", "666");
+            inputData.Add("ImpNeto", "0");
+            inputData.Add("ImpOpEx", "0");
+            inputData.Add("ImpTrib", "0");
+            inputData.Add("ImpIVA", "0");
+            inputData.Add("FchServDesde", "");
+            inputData.Add("FchServHasta", "");
+            inputData.Add("FchVtoPago", "");
+            inputData.Add("MonCotiz", "1");
+
+
+
+
+            //
+            // FECompUltimoAutorizado - FECompConsultar - FEParamGetPtosVenta - FECAESolicitar
+            //
 
             WSEB wseb = new WSEB();
             watch.Start();
-            wseb.executeOperation(inputData, "FEParamGetTiposTributos");
+            wseb.executeOperation(inputData, "FECAESolicitar");
             watch.Stop();
             Console.WriteLine($"request attempt took {watch.ElapsedMilliseconds} ms");
 
 
-            //WSLI wsli = new WSLI() { CERT = "C:\\Users\\54112\\Desktop\\proyectoAfip\\cert.pfx" };
+            //WSLI wsli = new WSLI() { CERT = "C:\\Users\\54112\\Desktop\\certs\\cert.pfx" };
             //watch.Start();
             //wsli.LogIn();
             //watch.Stop();
